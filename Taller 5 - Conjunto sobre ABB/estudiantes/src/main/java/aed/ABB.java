b88dcdd57435c885d5b3919b5e3d279f058951ef
TobiPerel <pereltobias@gmail.com>
2024-05-29 11:45:31 -0300

    finalizacion de talleres

--- Taller 5 - Conjunto sobre ABB/estudiantes/src/main/java/aed/ABB.java
+++ Taller 5 - Conjunto sobre ABB/estudiantes/src/main/java/aed/ABB.java
@@ -6,23 +6,21 @@ import java.util.*;
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     // Agregar atributos privados del Conjunto
-    private Nodo<T> raiz;
-    private int cardinal;
-    private class Nodo {
-        private T dato;
-        private Nodo<T> izquierdo;
-        private Nodo<T> derecho;
+    private Nodo<T> raiz = null;
+    private int cardinal = 0;
 
-        public Nodo(T dato) {
+    private class Nodo<T> {
+        T dato;
+        Nodo<T> izquierdo;
+        Nodo<T> derecho;
+        Nodo(T dato) {
             this.dato = dato;
-            this.izquierdo = null;
-            this.derecho = null;
         }
     }
 
     public ABB() {
-        this.raiz = null;
         this.cardinal = 0;
+
     }
 
     public int cardinal() {
@@ -94,34 +92,104 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         return false;
     }
 
-    public void eliminar(T elem){
-        if(pertenece(elem)){
-            raiz = eliminarElemento(raiz, elem);
+    public void eliminar(T elem) {
+        if (pertenece(elem)) {
             this.cardinal--;
+            raiz = eliminarNodo(raiz, elem);
         }
     }
-
-    public String toString(){ // muestra los elementos del conjunto en orden
-        String str = "";
-        while (this.raiz != null) {
-            str = "{" + this.raiz.dato;
-            str += this.toString(this.raiz.izquierdo);
-            str += this.toString(this.raiz.derecho);
-            str += "}";
+    
+    private Nodo<T> eliminarNodo(Nodo<T> nodo, T elem) {
+        if (nodo == null) {
+            return nodo;
         }
+    
+        int comparacion = elem.compareTo(nodo.dato);
+    
+        if (comparacion < 0) {
+            nodo.izquierdo = eliminarNodo(nodo.izquierdo, elem);
+        } else if (comparacion > 0) {
+            nodo.derecho = eliminarNodo(nodo.derecho, elem);
+        } else { 
+            if (nodo.izquierdo == null && nodo.derecho == null) {
+                return null;
+            }
+    
+            if (nodo.izquierdo == null) {
+                return nodo.derecho;
+            } else if (nodo.derecho == null) {
+                return nodo.izquierdo;
+            }
+    
+            Nodo<T> reemplazo = buscarMinimo(nodo.derecho); 
+            nodo.dato = reemplazo.dato; 
+    
+            nodo.derecho = eliminarNodo(nodo.derecho, reemplazo.dato);
+        }
+    
+        return nodo;
+    }
+    
+    private Nodo<T> buscarMinimo(Nodo<T> nodo) {
+        if (nodo.izquierdo == null) {
+            return nodo;
+        }
+        return buscarMinimo(nodo.izquierdo);
+    }
+        
+
+    public String toString() {
+        return "{" + toString(raiz) + "}";
+    }
+    
+    private String toString(Nodo<T> nodo) {
+        if (nodo == null) {
+            return "";
+        }
+    
+        String str = "";
+        if (nodo.izquierdo != null) {
+            str += toString(nodo.izquierdo) + ",";
+        }
+    
+        str += nodo.dato.toString();
+    
+        if (nodo.derecho != null) {
+            str += "," + toString(nodo.derecho);
+        }
+    
+        return str;
     }
 
     private class ABB_Iterador implements Iterador<T> {
-        private Nodo _actual;
+        private Stack<Nodo<T>> pila = new Stack<>();
 
-        public boolean haySiguiente() {     
-            throw new UnsupportedOperationException("No implementada aun");
+    public ABB_Iterador() {
+        empujarIzquierda(raiz);
+    }
 
+    private void empujarIzquierda(Nodo<T> nodo) {
+        while (nodo != null) {
+            pila.push(nodo);
+            nodo = nodo.izquierdo;
         }
-    
-        public T siguiente() {
-            throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public boolean haySiguiente() {
+        return !pila.isEmpty();
+    }
+
+    public T siguiente() {
+        if (!haySiguiente()) {
+            throw new NoSuchElementException();
         }
+        Nodo<T> nodo = pila.pop();
+        T resultado = nodo.dato;
+        if (nodo.derecho != null) {
+            empujarIzquierda(nodo.derecho);
+        }
+        return resultado;
+    }
     }
 
     public Iterador<T> iterador() {
---
+++ Taller 5 - Conjunto sobre ABB/estudiantes/src/main/java/aed/taller5.java
@@ -0,0 +1,160 @@
+// package aed;
+
+import java.util.*;
+
+public class taller5 {
+    public static void main(String[] args) {
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 0
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(42);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 1
+        // System.out.println("Pertenece 42: " + conjunto.pertenece(42)); // true
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(2);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 1
+        // conjunto.insertar(1);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 2
+        // conjunto.insertar(3);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 3
+        // conjunto.insertar(4);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 4
+        // conjunto.insertar(4);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 4
+        // System.out.println("Pertenece 4: " + conjunto.pertenece(4)); // true
+        // System.out.println("Pertenece 5: " + conjunto.pertenece(5)); // false
+        // System.out.println("Pertenece 1: " + conjunto.pertenece(1)); // true
+
+        // ABB<String> conjunto = new ABB<String>();
+        // System.out.println("Jujuy".compareTo("La Pampa") < 0);
+        // System.out.println("Jujuy".compareTo("Chubut") > 0);
+        // conjunto.insertar("La Pampa");
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 1
+        // System.out.println("Minimo: " + conjunto.minimo()); // La Pampa
+        // System.out.println("Maximo: " + conjunto.maximo()); // La Pampa
+        // conjunto.insertar("Chubut");
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 2
+        // System.out.println("Minimo: " + conjunto.minimo()); // Chubut
+        // System.out.println("Maximo: " + conjunto.maximo()); // La Pampa
+        // conjunto.insertar("Formosa");
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 3
+        // System.out.println("Minimo: " + conjunto.minimo()); // Chubut
+        // System.out.println("Maximo: " + conjunto.maximo()); // La Pampa
+        // conjunto.insertar("Catamarca");
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 4
+        // System.out.println("Minimo: " + conjunto.minimo()); // Catamarca
+        // System.out.println("Maximo: " + conjunto.maximo()); // La Pampa
+        // conjunto.insertar("Jujuy"); 
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 5
+        // System.out.println("Minimo: " + conjunto.minimo()); // Catamarca
+        // System.out.println("Maximo: " + conjunto.maximo()); // La Pampa
+        // System.out.println("Pertenece Catamarca: " + conjunto.pertenece("Catamarca")); // true
+        // System.out.println("Pertenece Buenos aires: " + conjunto.pertenece("Buenos Aires")); // true
+        // System.out.println("Pertenece Jujuy: " + conjunto.pertenece("Jujuy")); // true
+        
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(6);
+        // conjunto.insertar(7);
+        // conjunto.eliminar(6);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 2
+        // System.out.println("Minimo: " + conjunto.minimo()); // 5
+        // System.out.println("Maximo: " + conjunto.maximo()); // 7
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(4);
+        // conjunto.insertar(7);
+        // conjunto.insertar(6);
+        // conjunto.insertar(8);
+        // conjunto.eliminar(7);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 4
+        // System.out.println("Minimo: " + conjunto.minimo()); // 4
+        // System.out.println("Maximo: " + conjunto.maximo()); // 8
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(4);
+        // conjunto.insertar(7);
+        // conjunto.insertar(6);
+        // conjunto.insertar(8);
+        // conjunto.eliminar(5);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 4
+        // System.out.println("Minimo: " + conjunto.minimo()); // 4
+        // System.out.println("Maximo: " + conjunto.maximo()); // 8
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(4);
+        // conjunto.insertar(20);
+        // conjunto.insertar(15);
+        // conjunto.insertar(12);
+        // conjunto.insertar(16);
+        // conjunto.insertar(24);
+        // conjunto.insertar(22);
+        // conjunto.insertar(25);
+        // conjunto.eliminar(20);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 8
+        // System.out.println("Minimo: " + conjunto.minimo()); // 4
+        // System.out.println("Maximo: " + conjunto.maximo()); // 25
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(4);
+        // conjunto.insertar(20);
+        // conjunto.insertar(15);
+        // conjunto.insertar(12);
+        // conjunto.insertar(24);
+        // conjunto.insertar(22);
+        // conjunto.insertar(25);
+        // conjunto.insertar(19);
+        // conjunto.insertar(21);
+        // conjunto.eliminar(20);
+        // System.out.println("Cardinal: " + conjunto.cardinal()); // 9
+        // System.out.println("Minimo: " + conjunto.minimo()); // 4
+        // System.out.println("Maximo: " + conjunto.maximo()); // 25
+        // System.out.println(conjunto.toString());
+
+        // ABB<Integer> conjunto = new ABB<Integer>();
+        // conjunto.insertar(5);
+        // conjunto.insertar(4);
+        // conjunto.insertar(20);
+        // conjunto.insertar(15);
+        // conjunto.insertar(12);
+        // conjunto.insertar(16);
+        // conjunto.insertar(24);
+        // conjunto.insertar(22);
+        // conjunto.insertar(25);
+        // System.out.println(conjunto.toString());
+        // Iterador<Integer> iterador = conjunto.iterador();
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+        // System.out.println(iterador.siguiente());
+
+        // ABB<Integer> c = new ABB<Integer>();
+        // c.insertar(5);
+        // c.insertar(4);
+        // c.insertar(7);
+        // c.insertar(6);
+        // c.insertar(8);
+        // System.out.println(c.toString());
+        // c.eliminar(5);
+        // c.eliminar(7);
+        // System.out.println(c.toString());
+
+        Integer NCLAVES = 1000; 
+
+        private Integer clave(Integer i) {        
+            return NCLAVES * ((i * i - 100 * i) % NCLAVES) + i;
+        }
+    }
+}
